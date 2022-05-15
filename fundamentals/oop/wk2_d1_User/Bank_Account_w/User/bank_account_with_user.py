d2179ef7d049122eacdfbd61e9dfa39cd745d0e0
Rob_Rawl <34248558+Robert-Rawl@users.noreply.github.com>
2022-05-15 14:34:12 -0500

    update2

--- fundamentals/oop/wk2_d1_User/Bank_Account_w/User/bank_account_with_user.py
+++ fundamentals/oop/wk2_d1_User/Bank_Account_w/User/bank_account_with_user.py
@@ -6,13 +6,13 @@ class BankAccount:
         self.int_rate = int_rate
         self.balance = balance
         # don't worry about user info here; we'll involve the User class soon
-        #BankAccount.accounts.append(self)
+        BankAccount.accounts.append(self)
 
 
     def deposit(self, amount):
         #Add a deposit method to the BankAccount class
         self.balance += amount
-        return(self)
+        return self
 
 
 
@@ -23,14 +23,14 @@ class BankAccount:
         else:
             print('Insuffiecient Funds: Charging $5')
             self.balance -= 5
-        return(self)
+        return self
 
 
 
     def display_account_info(self):
         # add a display_account_info method to Bank account class
-        print(f'Your account balance is: {self.balance}')
-        return(self)
+        return f'{self.balance}'
+        
 
 
     def yield_interest(self):
@@ -39,11 +39,35 @@ class BankAccount:
             self.balance =  self.balance + (self.balance * self.int_rate)
         return(self)
 
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+#Update the User class __init__ method
 class User:
-    def __init__(self, name, email):
+
+    def __init__(self, name):
         self.name = name
-        self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
-        
+        self.account = {
+            'checking' : BankAccount(.02,500),
+            'savings' : BankAccount(.035, 2000)
+        }
+
+    def display_user_balance(self):
+        print(f'User: {self.name}, Balance: {self.account.display_account_info()}')
+        return self
+
+    def transfer_money(self,amount,user):
+
+        self.amount -= amount
+        user.amount += amount
+        self.display_user_balance()
+        user.display_user_balance()
+        return self
+
+rob = User('Rob')
+#rob.account.deposit(100)
+rob.display_user_balance()
 
 
