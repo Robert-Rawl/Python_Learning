de38617b4df806210a038fd262525f3d3ad5a946
Rob_Rawl <34248558+Robert-Rawl@users.noreply.github.com>
2022-05-24 16:43:53 -0500

    flask_v_2

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -1,9 +1,15 @@
 from distutils.log import debug
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from flask import Flask, render_template
 
-def hello_world():
-    return "Hello World!"
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html", phrase='hello', times=5)
+
+@app.route('/hello/<string:word>/<int:num>')
+def hello(word,num):
+    return render_template('hello.html',word=word, num=num)
+
 if __name__ == "__main__":
     app.run(debug=True)
